7e43cc43c637d564a9f482f9d7ee5bee0575d2e1
shrimp <luke560@users.noreply.github.com>
2026-09-08 15:45:42 -0600

    fix command blocker missing TFM command aliases (fr this time)

--- src/main/java/me/totalfreedom/totalfreedommod/blocking/command/CommandBlocker.java
+++ src/main/java/me/totalfreedom/totalfreedommod/blocking/command/CommandBlocker.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -20,6 +21,7 @@ import org.bukkit.event.server.ServerCommandEvent;
 import me.totalfreedom.totalfreedommod.FreedomService;
 import me.totalfreedom.totalfreedommod.blocking.command.CommandBlockerEntry.PatternToken;
 import me.totalfreedom.totalfreedommod.blocking.command.CommandBlockerEntry.TokenType;
+import me.totalfreedom.totalfreedommod.cmd.internal.CommandHolder;
 import me.totalfreedom.totalfreedommod.config.ConfigEntry;
 import me.totalfreedom.totalfreedommod.util.FLog;
 import me.totalfreedom.totalfreedommod.util.FUtil;
@@ -134,13 +136,12 @@ public class CommandBlocker extends FreedomService
             final CommandBlockerEntry blockedCommandEntry = new CommandBlockerEntry(rank, action, commandName, patternTokens, message);
             registerEntry(commandName, blockedCommandEntry);
 
-            if (command != null)
-            {
-                for (String alias : command.getAliases())
-                {
-                    registerEntry(alias.toLowerCase(), blockedCommandEntry);
-                }
-            }
+            Stream.concat(command != null ? command.getAliases().stream() : Stream.empty(),
+                          CommandHolder.listAliases(commandName).stream())
+                  .map(String::toLowerCase)
+                  .distinct()
+                  .forEach(alias -> registerEntry(alias, blockedCommandEntry));
+
             loadedCount++;
         }
 
